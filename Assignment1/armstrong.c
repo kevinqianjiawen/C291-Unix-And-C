c19e33f3d73135a48ab618a97f0a6ceeb0636e97
Jiawen Qian <jiawqian@silo.soic.indiana.edu>
2016-05-19 16:02:15 -0400

    finish assignment1

--- Assignment1/armstrong.c
+++ Assignment1/armstrong.c
@@ -1,5 +1,30 @@
+#include<math.h>
 #include<stdio.h>
 void main(){
-  printf("Enter number");
+  int i;
+  printf("Enter number\n");
+  scanf("%d",&i);
+  //this part is to find the length of the number   
+    int temp = i;
+    int count;
+    while(temp > 0){
+      temp = temp/10;
+      count++;
+    }
   
+    //get the number which is the sum of digits rasied to the power total numbers of digits
+    int temp2 = i;
+    int num=0;
+    while(temp2 != 0){
+      num += pow((temp2 % 10),count);
+      temp2 = temp2 / 10;
+    }
+    
+    //check whether it is a armstrong number
+    if(i == num){
+      printf("The number is armstrong");
+    } else{
+      printf("The number is not armstrong");
+    }
+
 }
