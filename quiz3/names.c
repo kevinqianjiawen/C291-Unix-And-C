e4734963950aa95a55f72b310c3ff3d248b45676
Jiawen Qian <jiawqian@silo.soic.indiana.edu>
2016-06-13 22:52:20 -0400

    add comment

--- quiz3/names.c
+++ quiz3/names.c
@@ -8,6 +8,9 @@
 #include <string.h>
 #include "names.h"
 
+//we want to import the record based on the last name you type
+//the comand line is
+// ./importexport phone_list_in.csv phone_list_out.csv Kenn
 name_t * import_name_list (FILE * fp, char * name) {
   char line[100];
   int items_returned;
@@ -23,9 +26,12 @@ name_t * import_name_list (FILE * fp, char * name) {
     }
     else {
       if(field = strtok(line,",")) {
+	//compare two string
 	if(strncmp(field, name, strlen(name)) == 0){
+	  
 	  current = (name_t *) malloc(sizeof(name_t));
 	  current->next = NULL;
+	  
 	  strcpy(current->lname,field);
 	  if (field = strtok(NULL,",")) {
 	    strcpy(current->fname,field);
@@ -35,16 +41,17 @@ name_t * import_name_list (FILE * fp, char * name) {
 	    strcpy(current->phone_number,field);
 	  }
 	  else break;
-      if (last) {
-	last->next = current;
-	last = current;
-      }
-      else {
-	first = current;
-	last = current;
-       }
+	  if (last) {
+	    last->next = current;
+	    last = current;
+	  }
+	  else {
+	    first = current;
+	    last = current;
+	  }
 	}
-      }else break;
+      }
+      else break;
     }
   }
   return(first);
